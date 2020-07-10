ab798b4a47243dde7437be0b472b065ff6718575
TatevikAl <67757997+TatevikAl@users.noreply.github.com>
2020-07-10 21:01:02 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Dynamic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter mathematical expression as an input");
+            String value = Console.ReadLine();
+            try
+            {
+                var result = new DataTable().Compute(value, null);
+                Console.WriteLine("result = " + result);
+            }
+            catch(Exception exeption)
+            {
+                Console.WriteLine(
+                    $"Incorrect input: {exeption.Message}");
+            }
+            Console.ReadLine();
+              
+
+        }
+    }
+}
